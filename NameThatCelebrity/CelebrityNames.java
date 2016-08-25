3aa58798444e6a4352807a1eb6581207f2ab7bde
PrimalNox <42047@psdschools.org>
2016-08-25 12:43:01 -0600

    Name that

---
+++ NameThatCelebrity/CelebrityNames.java
@@ -0,0 +1,18 @@
+public class CelebrityNames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Allan Alda";
+		String num1 = s1.substring(2, 7);
+		String s2 = "John Wayne";
+		String num2 = s2.substring(2, 7);
+		String s3 = "Gregory Peck";
+		String num3 = s3.substring(2, 9);
+
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+
+	}
+
+}
